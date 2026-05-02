45765fcbbfc59a4b75a87d5c5fde272fad1ddd54
mayankasija <mayankasija719@gmail.com>
2026-05-02 23:05:14 +0530

    Commiting Task One

---
+++ task1.py
@@ -0,0 +1,16 @@
+#Create a list of 5 numbers
+#Print the 3rd element
+#Create a dictionary with your name, age, city
+#Print only the city
+
+
+my_list=[1,5,6,8,6]
+
+print(my_list[2])
+
+
+myDist={ "name" :"Mayank","age":25,"city":"Dhule"}
+
+
+
+print(myDist.get("city"))
