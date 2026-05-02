4b802cfcc0c8d48b5a107af690b854ad7f6e2f52
mayankasija <mayankasija7@gmail.com>
2026-05-03 03:25:55 +0530

    loops and task3
    loops and task3
    
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                 1,1 Top"/f/MyPython/PythonCore/.git/COMMIT_EDITMSG" [unix] 10L, 246B
    loops and task3
    
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                  1,1 Top
    loops and task3
    
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                    1,1 Top
    loops and task3
    
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                      1,1 Top
    loops and task3
    
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                        1,1 Top
    loops and task3
    
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                         1,1 Top
    loops and task3
    
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                          1,1 Top
    .git/COMMIT_EDITMSG [unix] (03:25 03/05/2026)                                                           10,1 Bot
         #

---
+++ loops.py
@@ -0,0 +1,22 @@
+
+print("Enter Your Age")
+
+age=int(input())
+
+
+if age<0:
+    age=age*-1
+
+if age < 18:
+    print("You are minor")
+else:
+    print("You are adult")
+ #for loop
+
+
+listd=[2,5,98,2]
+
+for i in listd:
+    print(i)
+
+
---
+++ task3.py
@@ -0,0 +1,40 @@
+#Print numbers from 1 to 10
+#Print even numbers from 1 to 20
+# Loop through a list and print sum of all elements
+
+for i in range(10):
+    print(i+1)
+
+
+
+for i in range(20):
+    i=i+1
+    if(i%2==0):
+       print(i)
+
+
+
+
+
+
+dynamicList=[]
+for i in range(5):
+    dynamicList.append(i)
+
+
+print(dynamicList)
+
+
+
+
+
+
+
+
+sampleList=[0,1,2,3,1]
+sumOfList=0
+for i in sampleList:
+    sumOfList+=i
+
+print("Sum of List is : ",sumOfList)
+
