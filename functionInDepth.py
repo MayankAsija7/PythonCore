b90682aad44fd498fc9c57d810dd8183b79ad452
mayankasija <mayankasija7@gmail.com>
2026-06-04 01:55:49 +0530

    commiting classmethods'

---
+++ functionInDepth.py
@@ -0,0 +1,53 @@
+class student:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    @classmethod
+    def dataUpdate(cls,a,b):
+        cls.a=a
+        cls.b=b
+
+    def disp(self):
+        print("Value of a is :",self.a)
+        print("Value of b is :",self.b)
+
+  
+
+
+print("Enter value for a")
+a=input()
+print("Enter value for b")
+b=input()
+
+obj=student(a,b)
+obj.disp()
+
+
+
+print("___________________________________________________________\n")
+
+print("Enter value for a")
+a=input()
+print("Enter value for b")
+b=input()
+obj2=student(a,b) 
+print("Data after in second obj ",obj2.disp())
+
+
+obj.dataUpdate(15,80)
+
+
+
+
+print("Data  in obj1 : ")
+obj.disp()
+
+print("Data  in obj2 : ")
+obj2.disp()
+
+
+
+# @classmethod modifies class-level data (student.a, student.b).
+# Your objects use instance data (self.a, self.b).
+# Therefore obj.dataUpdate(15,80) does not change obj.a and obj.b.
+# Use a normal instance method if you want to update an object's attributes.
---
+++ functionsTut.py
@@ -0,0 +1,52 @@
+#here is function example 
+
+
+
+print("Enter Your name")
+name=input()
+
+print("Hello ",name)
+
+#example 2 parameterized
+
+def add(a,b):
+    print("addition is : ",a+b)
+
+
+
+print("2 numbers")
+a=int(input())
+b=int(input())
+
+add(a,b)
+
+
+
+
+
+
+#method to reverse string 
+
+def revstr(str):
+    length=len(name)-1
+    tempstr=""
+
+    while length >=0:
+        tempstr+=name[length]
+        length=length-1
+    print(tempstr)
+
+
+
+
+
+
+
+#Reverse a string
+
+
+revstr(name)
+
+
+
+
