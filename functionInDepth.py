b8a82f45d5fb18dba45f224fe233328b9ae49c49
mayankasija <mayankasija7@gmail.com>
2026-06-30 02:07:30 +0530

    class level method implementation

--- functionInDepth.py
+++ functionInDepth.py
@@ -1,7 +1,7 @@
 class student:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
+    # def __init__(self,a,b):
+    #     self.a=a
+    #     self.b=b
     @classmethod
     def dataUpdate(cls,a,b):
         cls.a=a
@@ -19,7 +19,9 @@ a=input()
 print("Enter value for b")
 b=input()
 
-obj=student(a,b)
+obj=student()
+obj.a=a
+obj.b=b
 obj.disp()
 
 
@@ -30,9 +32,12 @@ print("Enter value for a")
 a=input()
 print("Enter value for b")
 b=input()
-obj2=student(a,b) 
-print("Data after in second obj ",obj2.disp())
-
+# obj2=student(a,b) 
+obj2=student()
+obj2.a=a
+obj2.b=b
+print("Data after in second obj ")
+obj2.disp()
 
 obj.dataUpdate(15,80)
 
@@ -46,8 +51,9 @@ print("Data  in obj2 : ")
 obj2.disp()
 
 
+print("Value Of A in class",student.a)
 
 # @classmethod modifies class-level data (student.a, student.b).
 # Your objects use instance data (self.a, self.b).
 # Therefore obj.dataUpdate(15,80) does not change obj.a and obj.b.
-# Use a normal instance method if you want to update an object's attributes.
+# Use a normal instance method if you want to update an object's attributes.1
